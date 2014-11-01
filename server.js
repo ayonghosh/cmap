6af861ef37b6fc7ded6aca0ffd01939be428294a
Ayon Ghosh <ayonghosh88@gmail.com>
2014-11-01 23:40:56 +0530

    Using MongoClient

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 //  OpenShift sample Node application
 var express = require('express');
 var fs      = require('fs');
-
+var mongodb = require('mongodb');
 
 /**
  *  Define the sample application.
@@ -32,7 +32,27 @@ var SampleApp = function() {
             self.ipaddress = "127.0.0.1";
         };
     };
+    
+    
+    // MongoDB
+    var MongoClient = mongodb.MongoClient;
 
+    // Connect to the db
+    MongoClient.connect("mongodb://admin:xLrJXj_5XjUc@localhost:27017/nodejs", function(err, db) {
+      if(err) { return console.dir(err); }
+
+//      var collection = db.collection('test');
+//      var doc1 = {'hello':'doc1'};
+//      var doc2 = {'hello':'doc2'};
+//      var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}];
+//
+//      collection.insert(doc1);
+//
+//      collection.insert(doc2, {w:1}, function(err, result) {});
+//
+//      collection.insert(lotsOfDocs, {w:1}, function(err, result) {});
+
+    });
 
     /**
      *  Populate the cache.
